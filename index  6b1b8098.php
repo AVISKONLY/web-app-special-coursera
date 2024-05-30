f5802a974d0c30b231b8eacc9616b66282a0e11b
AVISKONLY <117367231+AVISKONLY@users.noreply.github.com>
2024-05-30 19:17:42 +0530

    Update index  6b1b8098.php

--- index  6b1b8098.php
+++ index  6b1b8098.php
@@ -1,22 +1,15 @@
-required_once "https://www.wa4e.com/solutions/rps/login.php"
 <!DOCTYPE html>
 <html>
 <head>
-<title>Charles Severance - Rock Paper Scissors</title>
-
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
-<!-- Custom styles for this template -->
-<link href="starter-template.css" rel="stylesheet">
-
+<title>Charles Severance - Broken Rock Paper Scissors</title>
+<?php require_once "bootstrap.php"; ?>
 </head>
 <body>
 <div class="container">
-<h1>Welcome to Rock Paper Scissors</h1>
+<h1>Welcome to Broken Rock Paper Scissors</h1>
+<p><strong>Note:</strong> This sample code is only
+partially done and serves only as a starting point for the assignment.
+</p>
 <p>
 <a href="login.php">Please Log In</a>
 </p>
@@ -24,7 +17,8 @@ required_once "https://www.wa4e.com/solutions/rps/login.php"
 Attempt to go to 
 <a href="game.php">game.php</a> without logging in - it should fail with an error message.
 <p>
-<a href="https://www.wa4e.com/assn/rps/" target="_blank">Specification for this Application</a>
+<a href="http://www.wa4e.com/code/rps.zip"
+ target="_blank">Source Code for this Application</a>
 </p>
 </div>
 </body>
